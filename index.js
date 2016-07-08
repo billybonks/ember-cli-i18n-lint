1314148fb81e68c87ba4d496c2eeda45599dfb7a
billybonks <sebastienstettler@gmail.com>
2016-07-08 15:53:47 +0200

    initial commit

--- index.js
+++ index.js
@@ -2,5 +2,12 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-i18n-lint'
+  name: 'ember-cli-i18n-lint',
+
+  included: function(app) {
+  },
+
+  lintTree: function(type, tree) {
+    console.log(this.app.trees)
+  }
 };
